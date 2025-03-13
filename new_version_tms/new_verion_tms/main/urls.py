361a07048929194a4d5919dcb72f29cb6fa99528
Johny <jonnycrazy776@gmail.com>
2025-03-13 20:31:55 +0500

    planed_added

--- new_version_tms/new_verion_tms/main/urls.py
+++ new_version_tms/new_verion_tms/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home)
+    path("", views.home),
+    path("planned_loads", views.planned_loads)
 ]
--- new_version_tms/new_verion_tms/main/views.py
+++ new_version_tms/new_verion_tms/main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def planned_loads(request):
+    return render(request, 'main/planned_loads.html')
